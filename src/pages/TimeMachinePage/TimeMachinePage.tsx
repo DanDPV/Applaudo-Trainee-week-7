312bc644cc5698f9d78f896fb6434050e6ac9df8
Vicente De Paz <danconec7ion@gmail.com>
2020-12-14 12:52:10 -0600

    :recycle: Use TimeMachineBoard component

--- src/pages/TimeMachinePage/TimeMachinePage.tsx
+++ src/pages/TimeMachinePage/TimeMachinePage.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import TimeMachineBoard from 'components/TimeMachineBoard';
 
 const TimeMachinePage = () => (
   <div className="page-content">
     <h1>Time machine</h1>
     <div className="container">
-      <div className="board">a</div>
+      <div className="board">
+        <TimeMachineBoard />
+      </div>
       <div className="info">o</div>
     </div>
   </div>
