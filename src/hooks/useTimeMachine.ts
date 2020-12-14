387d3fefc1b80244aa1b0521e68c90efc5eed9d9
Vicente De Paz <danconec7ion@gmail.com>
2020-12-14 17:55:39 -0600

    :construction: Store history in time machine hook

--- src/hooks/useTimeMachine.ts
+++ src/hooks/useTimeMachine.ts
@@ -2,9 +2,11 @@ import { useEffect, useRef } from 'react';
 
 const useTimeMachine = <T> (value: T) => {
   const ref = useRef<T>();
+  const history = useRef<T[]>([]);
 
   useEffect(() => {
     ref.current = value;
+    history.current.unshift(value);
   }, [value]);
 
   return [ref.current];
