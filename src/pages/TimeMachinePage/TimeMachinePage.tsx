d62f16b1a42beb18fe0abbcc058f6421b4ddf75e
Vicente De Paz <danconec7ion@gmail.com>
2020-12-14 15:40:21 -0600

    :recycle: Pass size through props between components

--- src/pages/TimeMachinePage/TimeMachinePage.tsx
+++ src/pages/TimeMachinePage/TimeMachinePage.tsx
@@ -7,7 +7,7 @@ const TimeMachinePage = () => (
     <h1>Time machine</h1>
     <div className="container">
       <div className="board">
-        <TimeMachineBoard />
+        <TimeMachineBoard size={4} />
       </div>
       <div className="info">o</div>
     </div>
