c5772dfcc6e6cc8d1d945847023c6af407831218
Vicente De Paz <danconec7ion@gmail.com>
2020-12-14 14:12:42 -0600

    :construction: Add colors to squares

--- src/components/TimeMachineBoard/TimeMachineBoard.tsx
+++ src/components/TimeMachineBoard/TimeMachineBoard.tsx
@@ -4,36 +4,36 @@ import 'components/TimeMachineBoard/TimeMachineBoard.css';
 const TimeMachineBoard = () => {
   const [squares] = useState(Array(4 * 4).fill(null));
   console.log(squares);
-  const renderSquare = (i: number) => (
-    <button type="button" className="square" onClick={() => {}}>
+  const renderSquare = (i: number, bgColor: string) => (
+    <button type="button" style={{ backgroundColor: bgColor }} className="square" onClick={() => {}}>
       {i}
     </button>
   );
   return (
     <div>
       <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-        {renderSquare(3)}
+        {renderSquare(0, '#475a7a')}
+        {renderSquare(1, '#f466c8')}
+        {renderSquare(2, '#6a56fc')}
+        {renderSquare(3, '#5fc9f9')}
       </div>
       <div className="board-row">
-        {renderSquare(4)}
-        {renderSquare(5)}
-        {renderSquare(6)}
-        {renderSquare(7)}
+        {renderSquare(4, '#d3adc6')}
+        {renderSquare(5, '#788f43')}
+        {renderSquare(6, '#a480e1')}
+        {renderSquare(7, '#07d1c7')}
       </div>
       <div className="board-row">
-        {renderSquare(8)}
-        {renderSquare(9)}
-        {renderSquare(10)}
-        {renderSquare(11)}
+        {renderSquare(8, '#df5e51')}
+        {renderSquare(9, '#c9a43d')}
+        {renderSquare(10, '#3f01c0')}
+        {renderSquare(11, '#577bcd')}
       </div>
       <div className="board-row">
-        {renderSquare(12)}
-        {renderSquare(13)}
-        {renderSquare(14)}
-        {renderSquare(15)}
+        {renderSquare(12, '#e42e5f')}
+        {renderSquare(13, '#a7762b')}
+        {renderSquare(14, '#cdb852')}
+        {renderSquare(15, '#7d3f99')}
       </div>
     </div>
   );
