f479887dcad8498fb77709ccb7de84232c6d447e
Vicente De Paz <danconec7ion@gmail.com>
2020-12-14 12:20:08 -0600

    :construction: Add TimeMachinePage base

---
+++ src/pages/TimeMachinePage/TimeMachinePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TimeMachinePage = () => (
+  <div className="page-content">
+    <h1>Time machine</h1>
+    <div className="container">
+      <div className="board">a</div>
+      <div className="info">o</div>
+    </div>
+  </div>
+);
+
+export default TimeMachinePage;
