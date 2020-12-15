e962d50030c9f002ddf72c6f82caa4cdb6a1ad5b
Vicente De Paz <danconec7ion@gmail.com>
2020-12-14 18:09:07 -0600

    :sparkles: Add function that return previous value at certaint period of time

--- src/hooks/useTimeMachine.ts
+++ src/hooks/useTimeMachine.ts
@@ -1,15 +1,20 @@
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
-const useTimeMachine = <T> (value: T) => {
+const useTimeMachine = <T>(value: T) => {
   const ref = useRef<T>();
   const history = useRef<T[]>([]);
+
+  const getPreviousValue = useCallback(
+    (position: number) => history.current[position],
+    [value],
+  );
 
   useEffect(() => {
     ref.current = value;
     history.current.unshift(value);
   }, [value]);
 
-  return [ref.current];
+  return [ref.current, getPreviousValue];
 };
 
 export default useTimeMachine;
