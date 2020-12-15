5d2788e4e4a5f6d1f8c005e699bdb591de994dce
Vicente De Paz <danconec7ion@gmail.com>
2020-12-15 16:08:12 -0600

    :construction: Add TicTacToeBoard base component

---
+++ src/components/TicTacToeBoard/TicTacToeBoard.tsx
@@ -0,0 +1,37 @@
+/* eslint no-unused-vars: 0 */
+import React, { useState } from 'react';
+import TicTacToeBoardButton from 'components/TicTacToeBoardButton/TicTacToeBoardButton';
+
+interface ITicTacToeSquareState {
+  id: string;
+  value: boolean;
+}
+
+interface ITicTacToeBoard {
+  squares: ITicTacToeSquareState[];
+  isTraveling: boolean;
+  handleClick(position: number): void;
+}
+
+const TicTacToeBoard = ({ squares, isTraveling, handleClick }: ITicTacToeBoard) => {
+  const [color] = useState<string>('gray');
+
+  return (
+    <div>
+      <div className="board-grid">
+        {squares.map((state: ITicTacToeSquareState, position: number) => (
+          <TicTacToeBoardButton
+            key={state.id}
+            position={position}
+            bgColor={color}
+            checked={state.value}
+            handleClick={handleClick}
+            isTraveling={isTraveling}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TicTacToeBoard;
