a5d2b123a679700d5e4a292b5b4b5bf2757d7420
Vicente De Paz <danconec7ion@gmail.com>
2020-12-14 18:16:00 -0600

    :recycle: Declare new variables on parent component

--- src/pages/TimeMachinePage/TimeMachinePage.tsx
+++ src/pages/TimeMachinePage/TimeMachinePage.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TimeMachineBoard from 'components/TimeMachineBoard/TimeMachineBoard';
 import 'pages/TimeMachinePage/TimeMachinePage.css';
 
-const TimeMachinePage = () => (
-  <div className="page-content">
-    <h1>Time machine</h1>
-    <div className="container">
-      <div className="board">
-        <TimeMachineBoard size={4} />
+const TimeMachinePage = () => {
+  const size = 4;
+  const [squares, setSquares] = useState<boolean[]>(Array(size * size).fill(false));
+  const handleClick = (i: number) => {
+    const newSquares = Array(size * size).fill(false) as boolean[];
+    newSquares[i] = true;
+    setSquares(newSquares);
+  };
+  return (
+    <div className="page-content">
+      <h1>Time machine</h1>
+      <div className="container">
+        <div className="board">
+          <TimeMachineBoard squares={squares} handleClick={handleClick} />
+        </div>
+        <div className="info">o</div>
       </div>
-      <div className="info">o</div>
     </div>
-  </div>
-);
+  );
+};
 
 export default TimeMachinePage;
