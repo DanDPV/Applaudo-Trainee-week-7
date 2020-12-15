7cf65863fdd73181ca8ed1ed208c5ef9192f5144
Vicente De Paz <danconec7ion@gmail.com>
2020-12-15 16:08:29 -0600

    :construction: Add TicTacToePage base component

--- src/pages/TicTacToePage/TicTacToePage.tsx
+++ src/pages/TicTacToePage/TicTacToePage.tsx
@@ -1,5 +1,22 @@
+import TicTacToeBoard from 'components/TicTacToeBoard/TicTacToeBoard';
 import React from 'react';
 
-const TicTacToePage = () => <h1>Tic Tac Toe</h1>;
+const TicTacToePage = () => (
+  <div className="page-content">
+    <h1>Tic Tac Toe</h1>
+    <div className="container">
+      <div className="board">
+        <TicTacToeBoard
+          squares={squares}
+          handleClick={handleClick}
+          isTraveling={isTraveling}
+        />
+      </div>
+      <div className="info">
+        menu
+      </div>
+    </div>
+  </div>
+);
 
 export default TicTacToePage;
