e9938c38881c3e2ec5e8b1932c7fe0b4efdc78ba
Vicente De Paz <danconec7ion@gmail.com>
2020-12-15 16:23:14 -0600

    :construction: Add TicTacToePage base states

--- src/pages/TicTacToePage/TicTacToePage.tsx
+++ src/pages/TicTacToePage/TicTacToePage.tsx
@@ -1,22 +1,39 @@
 import TicTacToeBoard from 'components/TicTacToeBoard/TicTacToeBoard';
-import React from 'react';
+import ITicTacToeSquareState from 'interfaces/ITicTacToeSquareState';
+import React, { useState } from 'react';
 
-const TicTacToePage = () => (
-  <div className="page-content">
-    <h1>Tic Tac Toe</h1>
-    <div className="container">
-      <div className="board">
-        <TicTacToeBoard
-          squares={squares}
-          handleClick={handleClick}
-          isTraveling={isTraveling}
-        />
-      </div>
-      <div className="info">
-        menu
+const TicTacToePage = () => {
+  const initGameValues = [
+    { id: 0, value: null },
+    { id: 1, value: null },
+    { id: 2, value: null },
+    { id: 3, value: null },
+    { id: 4, value: null },
+    { id: 5, value: null },
+    { id: 6, value: null },
+    { id: 7, value: null },
+    { id: 8, value: null },
+  ];
+  const [isTraveling] = useState<boolean>(false);
+  const [squares] = useState<ITicTacToeSquareState[]>(initGameValues);
+
+  const handleClick = () => {};
+
+  return (
+    <div className="page-content">
+      <h1>Tic Tac Toe</h1>
+      <div className="container">
+        <div className="board">
+          <TicTacToeBoard
+            squares={squares}
+            handleClick={handleClick}
+            isTraveling={isTraveling}
+          />
+        </div>
+        <div className="info">menu</div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default TicTacToePage;
