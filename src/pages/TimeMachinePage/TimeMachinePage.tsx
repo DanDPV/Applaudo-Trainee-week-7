3e89229591328ac1591cb3d96e313eb585ca26cb
Vicente De Paz <danconec7ion@gmail.com>
2020-12-14 19:04:51 -0600

    :construction: Use useTimeMachine hook to store state

--- src/pages/TimeMachinePage/TimeMachinePage.tsx
+++ src/pages/TimeMachinePage/TimeMachinePage.tsx
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
 import TimeMachineBoard from 'components/TimeMachineBoard/TimeMachineBoard';
+import TimeMachineMenu from 'components/TimeMachineMenu/TimeMachineMenu';
+import useTimeMachine from 'hooks/useTimeMachine';
 import 'pages/TimeMachinePage/TimeMachinePage.css';
 
 const TimeMachinePage = () => {
   const size = 4;
+  const [currentPosition, setCurrentPosition] = useState<number>(0);
   const [squares, setSquares] = useState<boolean[]>(Array(size * size).fill(false));
+  const [, getPreviousValue, timeLength] = useTimeMachine(squares);
+
   const handleClick = (i: number) => {
     const newSquares = Array(size * size).fill(false) as boolean[];
     newSquares[i] = true;
     setSquares(newSquares);
+  };
+  const handleGetPrevious = (step: number) => {
+    setCurrentPosition(p => p + step);
+    console.log(timeLength);
+    setSquares(getPreviousValue(currentPosition + step));
   };
   return (
     <div className="page-content">
@@ -17,7 +27,13 @@ const TimeMachinePage = () => {
         <div className="board">
           <TimeMachineBoard squares={squares} handleClick={handleClick} />
         </div>
-        <div className="info">o</div>
+        <div className="info">
+          <TimeMachineMenu
+            getPreviousValue={handleGetPrevious}
+            currentPosition={currentPosition}
+            timeLength={timeLength}
+          />
+        </div>
       </div>
     </div>
   );
