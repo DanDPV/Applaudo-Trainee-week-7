edeb2e52d6ff9294f428eb49c13898587b85d972
Vicente De Paz <danconec7ion@gmail.com>
2020-12-16 09:56:58 -0600

    :construction: Add base TicTacToeMenu component

---
+++ src/components/TicTacToeMenu/TicTacToeMenu.tsx
@@ -0,0 +1,63 @@
+/* eslint no-unused-vars: 0 */
+import React, { useEffect, useState } from 'react';
+import 'components/TimeMachineMenu/TimeMachineMenu.css';
+
+interface ITicTacToeMenu {
+  currentPosition: number;
+  timeLength: number;
+  getPreviousValue(step: number): void;
+  handleResume(): void;
+}
+
+const TicTacToeMenu = ({
+  currentPosition,
+  timeLength,
+  getPreviousValue,
+  handleResume,
+}: ITicTacToeMenu) => {
+  const [isDisabledPrevious, setIsDisabledPrevious] = useState<boolean>(true);
+  const [isDisabledNext, setIsDisabledNext] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (
+      timeLength === 0
+      || currentPosition === (timeLength > 1 ? timeLength - 1 : timeLength)
+    ) {
+      setIsDisabledPrevious(true);
+    } else setIsDisabledPrevious(false);
+
+    if (currentPosition === 0) setIsDisabledNext(true);
+    else setIsDisabledNext(false);
+  }, [currentPosition, timeLength]);
+
+  return (
+    <div className="menu-container">
+      <button
+        type="button"
+        className="menu-option"
+        disabled={isDisabledPrevious}
+        onClick={() => getPreviousValue(+1)}
+      >
+        Previous
+      </button>
+      <button
+        type="button"
+        className="menu-option"
+        disabled={isDisabledNext}
+        onClick={() => getPreviousValue(-1)}
+      >
+        Next
+      </button>
+      <button
+        type="button"
+        className="menu-option"
+        disabled={isDisabledNext}
+        onClick={() => handleResume()}
+      >
+        Resume
+      </button>
+    </div>
+  );
+};
+
+export default TicTacToeMenu;
