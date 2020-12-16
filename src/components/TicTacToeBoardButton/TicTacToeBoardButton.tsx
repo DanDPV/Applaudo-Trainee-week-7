3230de4c5fd5388c7e264fb4fbc4bdae55eb12b5
Vicente De Paz <danconec7ion@gmail.com>
2020-12-16 09:43:10 -0600

    :recycle: Import TicTacToeSquareType

--- src/components/TicTacToeBoardButton/TicTacToeBoardButton.tsx
+++ src/components/TicTacToeBoardButton/TicTacToeBoardButton.tsx
@@ -1,10 +1,11 @@
 /* eslint no-unused-vars: 0 */
 import React from 'react';
+import TicTacToeSquareType from 'types/TicTacToeSquareType';
 import 'components/TicTacToeBoardButton/TicTacToeBoardButton.css';
 
 interface ITicTacToeBoardButton {
   position: number;
-  checked: 'x' | 'o' | null;
+  checked: TicTacToeSquareType;
   isTraveling: boolean;
   handleClick(position: number): void;
 }
