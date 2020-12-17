82a7592cec7b7476480fcf7231f3b7f67e49ef1e
Vicente De Paz <danconec7ion@gmail.com>
2020-12-17 10:28:55 -0600

    :recycle: Use lazy loading on routes

--- src/routers/AppRouter/AppRouter.tsx
+++ src/routers/AppRouter/AppRouter.tsx
@@ -1,19 +1,22 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import RouteNames from 'routers/RouteNames';
-import TimeMachinePage from 'pages/TimeMachinePage/TimeMachinePage';
-import TicTacToePage from 'pages/TicTacToePage/TicTacToePage';
 import Navbar from 'components/Navbar/Navbar';
+
+const TimeMachinePage = lazy(() => import('pages/TimeMachinePage/TimeMachinePage'));
+const TicTacToePage = lazy(() => import('pages/TicTacToePage/TicTacToePage'));
 
 const AppRouter = () => (
   <Router>
     <div className="page-container">
       <div className="content-wrap">
         <Navbar />
-        <Switch>
-          <Route exact path={RouteNames.TicTacToe} component={TicTacToePage} />
-          <Route path={RouteNames.Home} component={TimeMachinePage} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path={RouteNames.TicTacToe} component={TicTacToePage} />
+            <Route path={RouteNames.Home} component={TimeMachinePage} />
+          </Switch>
+        </Suspense>
       </div>
     </div>
   </Router>
