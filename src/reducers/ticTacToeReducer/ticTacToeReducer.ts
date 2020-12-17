fea5d5f0c376c3297e811920669b34d568e06d77
Vicente De Paz <danconec7ion@gmail.com>
2020-12-17 11:47:24 -0600

    :construction: Type payload and create basic actions

--- src/reducers/ticTacToeReducer/ticTacToeReducer.ts
+++ src/reducers/ticTacToeReducer/ticTacToeReducer.ts
@@ -1,18 +1,59 @@
 import ticTacToeActionsTypes from 'reducers/ticTacToeReducer/ticTacToeActionsTypes';
+import TicTacToeSquareType from 'types/TicTacToeSquareType';
 
 const ticTacToeReducer = (
   state = [],
   action: {
     type: ticTacToeActionsTypes;
-    payload: any;
-  }
+    payload: {
+      winner?: TicTacToeSquareType | string,
+      error?: string,
+      currentPosition?: number,
+      xIsNext?: boolean,
+      isTraveling?: boolean,
+      isReplaying?: boolean,
+      squares?: TicTacToeSquareType[],
+      historySquares?: TicTacToeSquareType[],
+    };
+  },
 ) => {
+  const {
+    winner,
+    error,
+    currentPosition,
+    xIsNext,
+    isTraveling,
+    isReplaying,
+    squares,
+    historySquares,
+  } = action.payload;
   switch (action.type) {
-    case 'add':
-      return [...state, action.payload];
+    case ticTacToeActionsTypes.SET_ERROR:
+      return { ...state, error };
 
-    case 'replace':
-      return action.payload;
+    case ticTacToeActionsTypes.SET_NEXT_PLAY:
+      return {
+        ...state,
+        xIsNext,
+        squares,
+        historySquares: squares,
+      };
+
+    case ticTacToeActionsTypes.MOVE_IN_TIME:
+      return {
+        ...state,
+        currentPosition,
+        isTraveling,
+        squares,
+      };
+
+    case ticTacToeActionsTypes.RESET:
+      return {
+        ...state,
+        currentPosition,
+        isTraveling,
+        squares,
+      };
 
     default:
       return state;
