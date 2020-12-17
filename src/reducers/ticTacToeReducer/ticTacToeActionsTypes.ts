14e9666c9ae6fa142fce6b214a165a544773ae88
Vicente De Paz <danconec7ion@gmail.com>
2020-12-17 11:25:20 -0600

    :sparkles: Add ticTacToeActionTypes that define the actions in the reducer

---
+++ src/reducers/ticTacToeReducer/ticTacToeActionsTypes.ts
@@ -0,0 +1,12 @@
+/* eslint no-shadow: 0 */
+/* eslint no-unused-vars: 0 */
+enum ticTacToeActionsTypes {
+  SET_ERROR = 'SET_ERROR',
+  SET_NEXT_PLAY = 'SET_NEXT_PLAY',
+  SET_WINNER = 'SET_WINNER',
+  MOVE_IN_TIME = 'MOVE_IN_TIME',
+  RESET = 'RESET',
+  REPLAY = 'REPLAY',
+}
+
+export default ticTacToeActionsTypes;
