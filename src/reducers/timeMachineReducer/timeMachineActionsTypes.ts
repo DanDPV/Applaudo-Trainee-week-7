ff29722210c1160539f0e879267f405776433556
Vicente De Paz <danconec7ion@gmail.com>
2020-12-17 15:09:50 -0600

    :sparkles: Add timeMachineActionsTypes

---
+++ src/reducers/timeMachineReducer/timeMachineActionsTypes.ts
@@ -0,0 +1,8 @@
+/* eslint no-shadow: 0 */
+/* eslint no-unused-vars: 0 */
+enum timeMachineActionsTypes {
+  SET_NEXT_OPTION = 'SET_NEXT_OPTION',
+  MOVE_IN_TIME = 'MOVE_IN_TIME',
+}
+
+export default timeMachineActionsTypes;
