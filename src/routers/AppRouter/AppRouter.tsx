bd54ec9a267ce7553e5419b98302359801cc6931
Vicente De Paz <danconec7ion@gmail.com>
2020-12-17 18:08:00 -0600

    :sparkles: Use Loading component while lazy loading

--- src/routers/AppRouter/AppRouter.tsx
+++ src/routers/AppRouter/AppRouter.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React, { Suspense, lazy } from 'react';
 import RouteNames from 'routers/RouteNames';
 import Navbar from 'components/Navbar/Navbar';
+import Loading from 'components/Loading/Loading';
 import 'routers/AppRouter/AppRouter.scss';
 
 const TimeMachinePage = lazy(() => import('pages/TimeMachinePage/TimeMachinePage'));
@@ -12,7 +13,7 @@ const AppRouter = () => (
     <div className="page-container">
       <div className="content-wrap">
         <Navbar />
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<Loading />}>
           <Switch>
             <Route exact path={RouteNames.TicTacToe} component={TicTacToePage} />
             <Route path={RouteNames.Home} component={TimeMachinePage} />
